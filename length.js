b866146a745459a9826ac6662bae661cc24054b4
unknown <89188599154B@gmail.com>
2022-10-06 20:40:29 +0300

    code func

--- length.js
+++ length.js
@@ -3,15 +3,15 @@ function length(array) {
         console.log("string");
     const arr = Array.from(array);
         console.log(arr);
-
+        let i = 0;
     for (let i = 0; i < arr.length; i++ ) {
-       let len = arr[i];
-         console.log(len)
+         console.log(arr[i]) 
+        
     };
-   
-
-
-
+    const array = [arr[i]];
+    const arrayLength = length(arr);
+    console.log(arrayLength);
+    return(arr[i]);
   } else if (Array.isArray(array)) {
     console.log("array");
     console.log("type is", typeof(array));
@@ -19,9 +19,10 @@ function length(array) {
     console.log("ошибка");
   }
 
-
+ 
 }
 
 length("sdsgsdgsgs");
+
 // length([1, 2, 3, 4]);
 // length(435335)
