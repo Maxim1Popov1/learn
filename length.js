13922d0cce5855a843e6a94306a18121f63f3992
unknown <89188599154B@gmail.com>
2022-10-07 15:48:33 +0300

    last

--- length.js
+++ length.js
@@ -1,28 +1,31 @@
 function length(array) {
+  console.log("array :>> ", array);
+  let count = 0;
   if (typeof array === "string") {
-        console.log("string");
+    console.log("string");
     const arr = Array.from(array);
-        console.log(arr);
-        let i = 0;
-    for (let i = 0; i < arr.length; i++ ) {
-         console.log(arr[i]) 
-        
-    };
-    const array = [arr[i]];
-    const arrayLength = length(arr);
-    console.log(arrayLength);
-    return(arr[i]);
+    
+    for (let i = 0; i < arr.length; i++) {
+      count = count + 1;
+    }
+    console.log("count :>> ", count);
+    return count;
+   
+    
   } else if (Array.isArray(array)) {
+    for (let i = 0; i < array.length; i++) {
+      count = count + 1;
+    }
+    console.log("count :>> ", count);
     console.log("array");
-    console.log("type is", typeof(array));
+    console.log("type is", typeof array);
+    return count;
   } else {
     console.log("ошибка");
   }
-
- 
 }
 
-length("sdsgsdgsgs");
-
-// length([1, 2, 3, 4]);
-// length(435335)
+//  const arrayLength = length("sdsgsdgsgs");
+const arrayLength = length([1, 2, 3, 4, 5, 6, 7, 8]);
+// const arrayLength = length(435335);
+console.log(arrayLength)
